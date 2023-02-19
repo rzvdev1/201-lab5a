1369a47960221c029ad529961a23a5209df06163
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-02-18 17:46:03 -0800

    function sumAndMultiply

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ function sum(a, b) {
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+//testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -35,7 +35,7 @@ function multiply(a, b) {
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5, 9);
+//testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -53,11 +53,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
   //eslint-disable-line
-  // let addition =
-  // let multiply =
-  // return
+  let addResult = sum(sum(a, b)[0], c)[0];
+  let multiplyResult = multiply(multiply(a, b)[0], c)[0];
+  let sumString = `${a} and ${b} and ${c} sum to ${addResult}.`;
+  return [
+    addResult,
+    multiplyResult,
+    sumString,
+    `The product of ${a} and ${b} and ${c} is ${multiplyResult}.`,
+  ];
 }
-
 // Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4, 7, 5);
 
